34bf3ebe8a62ed549076eac91f94f9ce61f5ba3b
kgregory413 <80074900+kgregory413@users.noreply.github.com>
2021-09-05 20:39:29 -0400

    Add files via upload

--- Module_1/GregoryHelloWorld.java
+++ Module_1/GregoryHelloWorld.java
@@ -1,1 +1,22 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by R. Krasso 2021
+    Additional modifications by Kylie Gregory 2021
+*/ 
+public class GregoryHelloWorld {
 
+    /**
+     * Method with two arguments, which are used to print the user's first and last name.
+     * @param firstName String 
+     * @param lastName String 
+     * @return prints the results to the console window.
+     */
+    public static void myName(String firstName, String lastName) {
+        System.out.println(String.format("Hello World from %s %s", firstName, lastName));
+    } // end myName
+
+    public static void main(String[] args) {
+        myName("Kylie", "Gregory"); // Call the myName() method with the values "Kylie" and "Gregory"
+    } // end main
+} // end GregoryHelloWorld
